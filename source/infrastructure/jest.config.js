1d7dd08c317a9ead9ed885b08f509e8f9655e638
Kamyar <kamyarz@amazon.com>
2026-05-12 10:22:54 -0400

    Testing dlt for demerzel

--- source/infrastructure/jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  roots: ["<rootDir>/test"],
-  testMatch: ["**/*.test.ts"],
-  transform: {
-    "^.+\\.tsx?$": "ts-jest",
-  },
-  coverageReporters: ["text", "clover", "json", ["lcov", { projectRoot: "../.." }]],
-};
